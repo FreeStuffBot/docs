879f7c81333aac1ebc3d676f05113da52393e37a
Andreas <git@maanex.me>
2025-05-05 22:22:58 +0200

    updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: '@nuxt-themes/docus',
+
   // devtools: { enabled: true },
 
   typescript: {
@@ -10,9 +11,12 @@ export default defineNuxtConfig({
       }
     }
   },
+
   modules: [
     // Remove it if you don't use Plausible analytics
     // https://github.com/nuxt-modules/plausible
     // '@nuxtjs/plausible'
-  ]
+  ],
+
+  compatibilityDate: '2025-05-05'
 })
